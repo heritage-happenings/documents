8f11f3e5f0541c36760e79c6d246365c17a2ebdb
Theo Armour <t.armour@gmail.com>
2025-05-17 17:00:07 -0700

    Add initial implementation of TooToo and GitHub Tree View features
    - Created tootooedit.html for editing and viewing files from GitHub repository.
    - Implemented github-tree-view.html and corresponding JavaScript for displaying repository structure.
    - Added main.js to manage core functionalities and configurations.
    - Introduced style-core.css for consistent styling across the application.
    - Developed tootoo.html as the main entry point for the application.
    - Integrated Showdown.js for Markdown processing.
    - Established navigation menu with dynamic content loading based on GitHub API.
    - Set up responsive design for better usability on various screen sizes.

---
+++ code/2025-05-17/main.js
@@ -0,0 +1,24 @@
+
+const COR = {
+
+    // Used by GFO ~ github file open
+    defaultFile: "README.md",
+    pathContent: "../../",
+
+    // Used by GTV ~ github tree view
+    user: "heritage-happenings",
+    repo: "heritage-happenings.github.io",
+    branch: "main",
+
+    urlSource: "https://github.com/heritage-happenings/heritage-happenings.github.io",
+    urlPathContent: "https://heritage-happenings.github.io/documents",
+    urlPushPath: "https://heritage-happenings.github.io/documents",
+
+    menuTitle: "Heritage Happenings",
+    version: "Version: 2025-05-17 14:29",
+ 
+    filterFolders: [], //[ "code" ],
+
+    ignoreFiles: [], //["404.html", "favicon.ico", "index.html", "readme.html"],
+}
+
