862cf8d65742cd84439c8f3391c1dc9225e1fb68
Theo Armour <t.armour@gmail.com>
2025-06-01 19:22:51 -0700

    Update main.js to adjust folder path handling and correct URL configurations for GitHub Pages

--- tootoo/2025-06-01/main.js
+++ tootoo/2025-06-01/main.js
@@ -1,3 +1,5 @@
+const folder = CONFIG.repo.includes( "github.io") ? "" : CONFIG.repo;
+
 const COR = {
   
   version: "2025-06-01",
@@ -19,9 +21,9 @@ const COR = {
 
   urlBaseAPI: `https://api.github.com/repos/${CONFIG.user}/${CONFIG.repo}/contents/`,
   urlSource: `https://github.com/${CONFIG.user}/${CONFIG.repo}/tree/main/`,
-  urlPathApps: `https://${CONFIG.user}.github.io/${CONFIG.repo}/tootoo/`,
-  urlPathContent: `https://${CONFIG.user}.github.io/${CONFIG.repo}/`,
-  urlPushPath: `https://${CONFIG.user}.github.io/${CONFIG.repo}/`,
+  urlPathApps: `https://${CONFIG.user}.github.io/tootoo/`,
+  urlPathContent: `https://${CONFIG.user}.github.io/${folder}/`,
+  urlPushPath: `https://${CONFIG.user}.github.io/${folder}/`,
 
   iconExternalLink: "<img src='https://pushme-pullyou.github.io/assets/svg/icon-external-link.svg' width=16 >",
   iconGitHub: `<img src="https://pushme-pullyou.github.io/assets/svg/mark-github.svg">`,
